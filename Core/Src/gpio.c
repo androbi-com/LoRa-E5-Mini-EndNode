f50405d730df8f026a1fc8142d743820158bb67c
Claus <denk@us.es>
2021-08-12 19:13:40 +0200

    first commit

---
+++ Core/Src/gpio.c
@@ -0,0 +1,93 @@
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+     PA3   ------> USART2_RX
+     PA2   ------> USART2_TX
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, DBG3_Pin|DBG4_Pin|LED2_Pin|DBG2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, DBG1_Pin|RF_CTRL1_Pin|RF_CTRL2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PBPin PBPin PBPin PBPin */
+  GPIO_InitStruct.Pin = DBG3_Pin|DBG4_Pin|LED2_Pin|DBG2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PAPin PAPin PAPin */
+  GPIO_InitStruct.Pin = DBG1_Pin|RF_CTRL1_Pin|RF_CTRL2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = BUT1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(BUT1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PAPin PAPin */
+  GPIO_InitStruct.Pin = USARTx_RX_Pin|USARTx_TX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ LoRaWAN/Target/radio_board_if.c
@@ -0,0 +1,288 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    radio_board_if.c
+  * @author  MCD Application Team
+  * @brief   This file provides an interface layer between MW and Radio Board
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "radio_board_if.h"
+
+/* USER CODE BEGIN Includes */
+#include "sys_app.h"
+/* USER CODE END Includes */
+
+/* External variables ---------------------------------------------------------*/
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Exported functions --------------------------------------------------------*/
+int32_t RBI_Init(void)
+{
+  /* USER CODE BEGIN RBI_Init_1 */
+
+  /* USER CODE END RBI_Init_1 */
+#if defined(USE_BSP_DRIVER)
+  /* Important note: BSP code is board dependent
+   * STM32WL_Nucleo code can be found
+   *       either in STM32CubeWL package under Drivers/BSP/STM32WLxx_Nucleo/
+   *       or at https://github.com/STMicroelectronics/STM32CubeWL/tree/main/Drivers/BSP/STM32WLxx_Nucleo/
+   * 1/ For User boards, the BSP/STM32WLxx_Nucleo/ directory can be copied and replaced in the project. The copy must then be updated depending:
+   *       on board RF switch configuration (pin control, number of port etc)
+   *       on TCXO configuration
+   *       on DC/DC configuration */
+  return BSP_RADIO_Init();
+#else
+  /* 2/ Or implement RBI_Init here */
+  int32_t retcode = 0;
+  /* USER CODE BEGIN RBI_Init_2 */
+
+  GPIO_InitTypeDef  gpio_init_structure = {0};
+
+  /* Configure the Radio Switch pin */
+  gpio_init_structure.Pin   = RF_SW_CTRL1_PIN;
+  gpio_init_structure.Mode  = GPIO_MODE_OUTPUT_PP;
+  gpio_init_structure.Pull  = GPIO_NOPULL;
+  gpio_init_structure.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+
+  HAL_GPIO_Init(RF_SW_CTRL1_GPIO_PORT, &gpio_init_structure);
+
+  gpio_init_structure.Pin = RF_SW_CTRL2_PIN;
+  HAL_GPIO_Init(RF_SW_CTRL2_GPIO_PORT, &gpio_init_structure);
+
+  HAL_GPIO_WritePin(RF_SW_CTRL2_GPIO_PORT, RF_SW_CTRL2_PIN, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(RF_SW_CTRL1_GPIO_PORT, RF_SW_CTRL1_PIN, GPIO_PIN_RESET);
+
+// #warning user to provide its board code or to call his board driver functions
+  /* USER CODE END RBI_Init_2 */
+  return retcode;
+#endif  /* USE_BSP_DRIVER  */
+}
+
+int32_t RBI_DeInit(void)
+{
+  /* USER CODE BEGIN RBI_DeInit_1 */
+
+  /* USER CODE END RBI_DeInit_1 */
+#if defined(USE_BSP_DRIVER)
+  /* Important note: BSP code is board dependent
+   * STM32WL_Nucleo code can be found
+   *       either in STM32CubeWL package under Drivers/BSP/STM32WLxx_Nucleo/
+   *       or at https://github.com/STMicroelectronics/STM32CubeWL/tree/main/Drivers/BSP/STM32WLxx_Nucleo/
+   * 1/ For User boards, the BSP/STM32WLxx_Nucleo/ directory can be copied and replaced in the project. The copy must then be updated depending:
+   *       on board RF switch configuration (pin control, number of port etc)
+   *       on TCXO configuration
+   *       on DC/DC configuration */
+  return BSP_RADIO_DeInit();
+#else
+  /* 2/ Or implement RBI_DeInit here */
+  int32_t retcode = 0;
+  /* USER CODE BEGIN RBI_DeInit_2 */
+  //RF_SW_CTRL3_GPIO_CLK_ENABLE();
+
+  /* Turn off switch */
+  HAL_GPIO_WritePin(RF_SW_CTRL1_GPIO_PORT, RF_SW_CTRL1_PIN, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(RF_SW_CTRL2_GPIO_PORT, RF_SW_CTRL2_PIN, GPIO_PIN_RESET);
+
+  /* DeInit the Radio Switch pin */
+  HAL_GPIO_DeInit(RF_SW_CTRL1_GPIO_PORT, RF_SW_CTRL1_PIN);
+  HAL_GPIO_DeInit(RF_SW_CTRL2_GPIO_PORT, RF_SW_CTRL2_PIN);
+
+//#warning user to provide its board code or to call his board driver functions
+  /* USER CODE END RBI_DeInit_2 */
+  return retcode;
+#endif  /* USE_BSP_DRIVER */
+}
+
+int32_t RBI_ConfigRFSwitch(RBI_Switch_TypeDef Config)
+{
+  /* USER CODE BEGIN RBI_ConfigRFSwitch_1 */
+
+  /* USER CODE END RBI_ConfigRFSwitch_1 */
+#if defined(USE_BSP_DRIVER)
+
+  /* Important note: BSP code is board dependent
+   * STM32WL_Nucleo code can be found
+   *       either in STM32CubeWL package under Drivers/BSP/STM32WLxx_Nucleo/
+   *       or at https://github.com/STMicroelectronics/STM32CubeWL/tree/main/Drivers/BSP/STM32WLxx_Nucleo/
+   * 1/ For User boards, the BSP/STM32WLxx_Nucleo/ directory can be copied and replaced in the project. The copy must then be updated depending:
+   *       on board RF switch configuration (pin control, number of port etc)
+   *       on TCXO configuration
+   *       on DC/DC configuration */
+  return BSP_RADIO_ConfigRFSwitch((BSP_RADIO_Switch_TypeDef) Config);
+#else
+  /* 2/ Or implement RBI_ConfigRFSwitch here */
+  int32_t retcode = 0;
+  /* USER CODE BEGIN RBI_ConfigRFSwitch_2 */
+  /* vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv */
+
+  switch (Config)
+  {
+    case RBI_SWITCH_OFF:
+    {
+      /* Turn off switch */
+      APP_LOG(TS_OFF, VLEVEL_M, "RF SWITCH: OFF\r\n");
+      HAL_GPIO_WritePin(RF_SW_CTRL1_GPIO_PORT, RF_SW_CTRL1_PIN, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(RF_SW_CTRL2_GPIO_PORT, RF_SW_CTRL2_PIN, GPIO_PIN_RESET);
+      break;
+    }
+    case RBI_SWITCH_RX:
+    {
+      /*Turns On in Rx Mode the RF Switch */
+      APP_LOG(TS_OFF, VLEVEL_M, "RF SWITCH: RX\r\n");
+      HAL_GPIO_WritePin(RF_SW_CTRL1_GPIO_PORT, RF_SW_CTRL1_PIN, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(RF_SW_CTRL2_GPIO_PORT, RF_SW_CTRL2_PIN, GPIO_PIN_RESET);
+      break;
+    }
+    case RBI_SWITCH_RFO_LP:
+    {
+      /*Turns On in Tx Low Power the RF Switch */
+      APP_LOG(TS_OFF, VLEVEL_M, "RF SWITCH: LP\r\n");
+      HAL_GPIO_WritePin(RF_SW_CTRL1_GPIO_PORT, RF_SW_CTRL1_PIN, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(RF_SW_CTRL2_GPIO_PORT, RF_SW_CTRL2_PIN, GPIO_PIN_SET);
+      break;
+    }
+    case RBI_SWITCH_RFO_HP:
+    {
+      /*Turns On in Tx High Power the RF Switch */
+      APP_LOG(TS_OFF, VLEVEL_M, "RF SWITCH: HP\r\n");
+      HAL_GPIO_WritePin(RF_SW_CTRL1_GPIO_PORT, RF_SW_CTRL1_PIN, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(RF_SW_CTRL2_GPIO_PORT, RF_SW_CTRL2_PIN, GPIO_PIN_SET);
+      break;
+    }
+    default:
+      break;
+  }
+
+  //#warning user to provide its board code or to call his board driver functions
+  /* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */
+  /* USER CODE END RBI_ConfigRFSwitch_2 */
+  return retcode;
+#endif  /* USE_BSP_DRIVER */
+}
+
+int32_t RBI_GetTxConfig(void)
+{
+  /* USER CODE BEGIN RBI_GetTxConfig_1 */
+
+  /* USER CODE END RBI_GetTxConfig_1 */
+#if defined(USE_BSP_DRIVER)
+  /* Important note: BSP code is board dependent
+   * STM32WL_Nucleo code can be found
+   *       either in STM32CubeWL package under Drivers/BSP/STM32WLxx_Nucleo/
+   *       or at https://github.com/STMicroelectronics/STM32CubeWL/tree/main/Drivers/BSP/STM32WLxx_Nucleo/
+   * 1/ For User boards, the BSP/STM32WLxx_Nucleo/ directory can be copied and replaced in the project. The copy must then be updated depending:
+   *       on board RF switch configuration (pin control, number of port etc)
+   *       on TCXO configuration
+   *       on DC/DC configuration */
+  return BSP_RADIO_GetTxConfig();
+#else
+  /* 2/ Or implement RBI_GetTxConfig here */
+  int32_t retcode = RBI_CONF_RFO;
+  /* USER CODE BEGIN RBI_GetTxConfig_2 */
+  // #warning user to provide its board code or to call his board driver functions
+  /* USER CODE END RBI_GetTxConfig_2 */
+  return retcode;
+#endif  /* USE_BSP_DRIVER */
+}
+
+int32_t RBI_IsTCXO(void)
+{
+  /* USER CODE BEGIN RBI_IsTCXO_1 */
+
+  /* USER CODE END RBI_IsTCXO_1 */
+#if defined(USE_BSP_DRIVER)
+  /* Important note: BSP code is board dependent
+   * STM32WL_Nucleo code can be found
+   *       either in STM32CubeWL package under Drivers/BSP/STM32WLxx_Nucleo/
+   *       or at https://github.com/STMicroelectronics/STM32CubeWL/tree/main/Drivers/BSP/STM32WLxx_Nucleo/
+   * 1/ For User boards, the BSP/STM32WLxx_Nucleo/ directory can be copied and replaced in the project. The copy must then be updated depending:
+   *       on board RF switch configuration (pin control, number of port etc)
+   *       on TCXO configuration
+   *       on DC/DC configuration */
+  return BSP_RADIO_IsTCXO();
+#else
+  /* 2/ Or implement RBI_IsTCXO here */
+  int32_t retcode = IS_TCXO_SUPPORTED;
+  /* USER CODE BEGIN RBI_IsTCXO_2 */
+  //#warning user to provide its board code or to call his board driver functions
+  /* USER CODE END RBI_IsTCXO_2 */
+  return retcode;
+#endif  /* USE_BSP_DRIVER  */
+}
+
+int32_t RBI_IsDCDC(void)
+{
+  /* USER CODE BEGIN RBI_IsDCDC_1 */
+
+  /* USER CODE END RBI_IsDCDC_1 */
+#if defined(USE_BSP_DRIVER)
+  /* Important note: BSP code is board dependent
+   * STM32WL_Nucleo code can be found
+   *       either in STM32CubeWL package under Drivers/BSP/STM32WLxx_Nucleo/
+   *       or at https://github.com/STMicroelectronics/STM32CubeWL/tree/main/Drivers/BSP/STM32WLxx_Nucleo/
+   * 1/ For User boards, the BSP/STM32WLxx_Nucleo/ directory can be copied and replaced in the project. The copy must then be updated depending:
+   *       on board RF switch configuration (pin control, number of port etc)
+   *       on TCXO configuration
+   *       on DC/DC configuration */
+  return BSP_RADIO_IsDCDC();
+#else
+  /* 2/ Or implement RBI_IsDCDC here */
+  int32_t retcode = IS_DCDC_SUPPORTED;
+  /* USER CODE BEGIN RBI_IsDCDC_2 */
+  // #warning user to provide its board code or to call his board driver functions
+  /* USER CODE END RBI_IsDCDC_2 */
+  return retcode;
+#endif  /* USE_BSP_DRIVER  */
+}
+
+/* USER CODE BEGIN EF */
+
+/* USER CODE END EF */
+
+/* Private Functions Definition -----------------------------------------------*/
+/* USER CODE BEGIN PrFD */
+
+/* USER CODE END PrFD */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
